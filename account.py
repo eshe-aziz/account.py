3a052e8970b68e8ff62681c60e5705280168d155
eshe-aziz <eshehaledhaziz@gmail.com>
2024-06-03 20:33:54 +0300

    add files

--- account.py
+++ account.py
@@ -1,88 +1,123 @@
+# # class Account:
+# #     def __init__(self, number, pin):
+# #         self.number = number
+# #         self.pin = pin
+# #         self.balance = 0
+
+
+# # >>> from account import Account
+# # >>> a = Account(number = 1234, pin = 6789)
+# # >>> a.pin
+# # 6789
+# # >>> a.number
+# # 1234
+# # >>> a.balance
+# # 
+
+
 # class Account:
 #     def __init__(self, number, pin):
 #         self.number = number
-#         self.pin = pin
-#         self.balance = 0
+#         self.__pin = pin
+#         self.__balance = 0
+
+#     def show_balance(self, pin):
+#         if pin == self.__pin:
+#             return self.__balance
+        
+#         else:
+#             return "Wrong pin"
 
 
-# >>> from account import Account
-# >>> a = Account(number = 1234, pin = 6789)
-# >>> a.pin
-# 6789
-# >>> a.number
-# 1234
-# >>> a.balance
-# 
+# # >>> my_account = Account(1234, 6789)
+# # >>> my_account.show_balance()
+# # >>> my_account.show_balance(678)
+# # 'Wrong pin'
+# # >>> my_account.show_balance(6789)
+# # 0
+
+#     def deposit(self, amount):
+#         self.__balance += amount
+
+#         print(f"{amount} has been deposited to your account. Your current balance is {self.__balance}")
+
+# # >>> from account import Account
+# # >>> a = Account(number = 1234, pin = 6789)
+# # >>> a.show_balance(pin = 678)
+# # 'Wrong pin'
+# # >>> a.deposit(amount = 5000)
+# # 5000 has been deposited to your account. Your current balance is 5000
+
+#     def withdraw(self, amount):
+#         if self.__balance >= amount:
+#             self.__balance -= amount
+#             return f"{amount} has been withdrawn from your account. Your current balance is {self.__balance}"
+        
+#         else:
+#             return f"Insufficient balance. Withdrawal declined."
+        
+# # >>> a.withdraw(amount = 2000)
+# # '2000 has been withdrawn from your account. Your current balance is 5000'
+
+
+#     def view_account_details(self, first_name, last_name):
+#         self.first_name = first_name
+#         self.last_name = last_name
+
+#         print(f"Dear {self.first_name} {self.last_name}, your balance is {self.__balance}")
+
+# # >>> a.view_account_details('Eshe', 'Aziz')
+# # Dear Eshe Aziz, your balance is 0
+
+#     def change_account(self, first_name, last_name):
+#         self.first_name = first_name
+#         self.last_name = last_name
+
+#         print(f"Account owner updated to {self.first_name} {self.last_name}")
+
+# #     >>> a.change_account('Moureen', 'Gitahi')
+# # Account owner updated to Moureen Gitahi
+
+#     def account_statement(self):
+        
+#         print(f"Available balance : {self.__balance}")
 
 
 class Account:
-    def __init__(self, number, pin):
+    def __init__(self, number, pin, owner_name, balance=0, overdraft_limit=0, interest_rate=0.0, minimum_balance=0):
         self.number = number
         self.__pin = pin
-        self.__balance = 0
+        self.owner_name = owner_name
+        self.balance = balance
+        self.overdraft_limit = overdraft_limit
+        self.interest_rate = interest_rate
+        self.minimum_balance = minimum_balance
+        self.transaction_history = []
+        self.frozen = False
+        self.__account_status = "Open"
 
+    def account_status(self):
+        return self.__account_status
+    
     def show_balance(self, pin):
         if pin == self.__pin:
-            return self.__balance
-        
+            return self.balance
         else:
-            return "Wrong pin"
-
-
-# >>> my_account = Account(1234, 6789)
-# >>> my_account.show_balance()
-# >>> my_account.show_balance(678)
-# 'Wrong pin'
-# >>> my_account.show_balance(6789)
-# 0
-
-    def deposit(self, amount):
-        self.__balance += amount
-
-        print(f"{amount} has been deposited to your account. Your current balance is {self.__balance}")
-
-# >>> from account import Account
-# >>> a = Account(number = 1234, pin = 6789)
-# >>> a.show_balance(pin = 678)
-# 'Wrong pin'
-# >>> a.deposit(amount = 5000)
-# 5000 has been deposited to your account. Your current balance is 5000
-
-    def withdraw(self, amount):
-        if self.__balance >= amount:
-            self.__balance -= amount
-            return f"{amount} has been withdrawn from your account. Your current balance is {self.__balance}"
+            return "Wrong Pin"
         
+    def view_account_details(self, pin):
+        if pin == self.__pin:
+            return f"Account Number: {self.number}\nOwner Name: {self.owner_name}\nBalance: {self.balance}\nStatus: {self.account_status()}"
         else:
-            return f"Insufficient balance. Withdrawal declined."
+            return "Wrong Pin"
         
-# >>> a.withdraw(amount = 2000)
-# '2000 has been withdrawn from your account. Your current balance is 5000'
-
-
-    def view_account_details(self, first_name, last_name):
-        self.first_name = first_name
-        self.last_name = last_name
-
-        print(f"Dear {self.first_name} {self.last_name}, your balance is {self.__balance}")
-
-# >>> a.view_account_details('Eshe', 'Aziz')
-# Dear Eshe Aziz, your balance is 0
-
-    def change_account(self, first_name, last_name):
-        self.first_name = first_name
-        self.last_name = last_name
-
-        print(f"Account owner updated to {self.first_name} {self.last_name}")
-
-#     >>> a.change_account('Moureen', 'Gitahi')
-# Account owner updated to Moureen Gitahi
-
-    def account_statement(self):
+    def change_account_owner(self, pin, new_owner_name):
+        if pin == self.__pin:
+            self.owner_name = new_owner_name
+            return "Owner's information is updated "
+        else:
+            return "Wrong Pin"
         
-        print(f"Available balance : {self.__balance}")
-
-
     def set_overdraft_limit(self, pin, new_limit):
         if pin == self.__pin:
             self.overdraft_limit = new_limit
@@ -102,6 +137,7 @@ class Account:
         else:
             return "Wrong Pin"
         
+        
     def unfreeze_account(self, pin):
         if pin == self.__pin:
             self.frozen = False
@@ -111,6 +147,8 @@ class Account:
         
     def transaction_history(self):
         return self.transaction_history
+    
+    
     def set_minimum_balance(self, pin, new_minimum_balance):
         if pin == self.__pin:
             self.minimum_balance = new_minimum_balance
@@ -133,6 +171,7 @@ class Account:
         else:
             return "Wrong Pin"
         
+        
     def close_account(self, pin):
         if pin == self.__pin:
             self.balance = 0
@@ -143,6 +182,19 @@ class Account:
             return "Account closed"
         else:
             return "Wrong Pin"
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
